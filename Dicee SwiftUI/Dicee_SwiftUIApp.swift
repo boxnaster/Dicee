4e2e6689d03c0f82277377c31a826ec8dc8c0c67
boxnaster <nastya3362@yandex.ru>
2024-04-23 21:31:23 +0200

    Initial Commit

---
+++ Dicee SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee SwiftUI
+//
+//  Created by Анастасия Тимофеева on 22.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
